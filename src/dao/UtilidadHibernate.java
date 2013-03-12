9e8f7f1ef5657998337c41aef4870d614b0badd8
Carlos <zengo120@gmail.com>
2013-03-12 12:16:53 +0100

    Añadida utilidad hibernate

---
+++ src/dao/UtilidadHibernate.java
@@ -0,0 +1,32 @@
+package dao;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class UtilidadHibernate {
+	/**
+	 * Startup Hibernate and provide access to the singleton SessionFactory
+	 */
+	private static SessionFactory sessionFactory;
+
+	static {
+		try {
+			sessionFactory = new Configuration().configure().buildSessionFactory();
+		} catch (Throwable ex) {
+			System.out.println(ex.getMessage());
+			throw new ExceptionInInitializerError(ex);
+		}
+	}	
+
+	public static SessionFactory getSessionFactory() {
+		// Alternatively, we could look up in JNDI here
+		return sessionFactory;
+	}
+
+	public static void shutdown() {
+		// Close caches and connection pools
+		getSessionFactory().close();
+	}
+
+}
+
