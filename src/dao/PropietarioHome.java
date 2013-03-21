c0c153ac73f26b6e7cd6b30de3b30f75cd0814cd
User <user@user-MacBook.(none)>
2013-03-21 18:23:32 +0100

    eliminados log

--- src/dao/PropietarioHome.java
+++ src/dao/PropietarioHome.java
@@ -3,12 +3,11 @@ package dao;
 // Generated 21-mar-2013 18:13:29 by Hibernate Tools 3.4.0.CR1
 
 import java.util.List;
+
 import javax.naming.InitialContext;
 
 import negocio.Propietario;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.hibernate.LockMode;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Example;
@@ -20,7 +19,6 @@ import org.hibernate.criterion.Example;
  */
 public class PropietarioHome {
 
-	private static final Log log = LogFactory.getLog(PropietarioHome.class);
 
 	private final SessionFactory sessionFactory = getSessionFactory();
 
@@ -29,97 +27,73 @@ public class PropietarioHome {
 			return (SessionFactory) new InitialContext()
 					.lookup("SessionFactory");
 		} catch (Exception e) {
-			log.error("Could not locate SessionFactory in JNDI", e);
 			throw new IllegalStateException(
 					"Could not locate SessionFactory in JNDI");
 		}
 	}
 
 	public void persist(Propietario transientInstance) {
-		log.debug("persisting Propietario instance");
 		try {
 			sessionFactory.getCurrentSession().persist(transientInstance);
-			log.debug("persist successful");
 		} catch (RuntimeException re) {
-			log.error("persist failed", re);
 			throw re;
 		}
 	}
 
 	public void attachDirty(Propietario instance) {
-		log.debug("attaching dirty Propietario instance");
 		try {
 			sessionFactory.getCurrentSession().saveOrUpdate(instance);
-			log.debug("attach successful");
 		} catch (RuntimeException re) {
-			log.error("attach failed", re);
 			throw re;
 		}
 	}
 
 	public void attachClean(Propietario instance) {
-		log.debug("attaching clean Propietario instance");
 		try {
 			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
-			log.debug("attach successful");
 		} catch (RuntimeException re) {
-			log.error("attach failed", re);
 			throw re;
 		}
 	}
 
 	public void delete(Propietario persistentInstance) {
-		log.debug("deleting Propietario instance");
 		try {
 			sessionFactory.getCurrentSession().delete(persistentInstance);
-			log.debug("delete successful");
 		} catch (RuntimeException re) {
-			log.error("delete failed", re);
 			throw re;
 		}
 	}
 
 	public Propietario merge(Propietario detachedInstance) {
-		log.debug("merging Propietario instance");
 		try {
 			Propietario result = (Propietario) sessionFactory
 					.getCurrentSession().merge(detachedInstance);
-			log.debug("merge successful");
 			return result;
 		} catch (RuntimeException re) {
-			log.error("merge failed", re);
 			throw re;
 		}
 	}
 
 	public Propietario findById(int id) {
-		log.debug("getting Propietario instance with id: " + id);
 		try {
 			Propietario instance = (Propietario) sessionFactory
 					.getCurrentSession().get("negocio.Propietario", id);
 			if (instance == null) {
-				log.debug("get successful, no instance found");
 			} else {
-				log.debug("get successful, instance found");
 			}
 			return instance;
 		} catch (RuntimeException re) {
-			log.error("get failed", re);
 			throw re;
 		}
 	}
 
 	public List findByExample(Propietario instance) {
-		log.debug("finding Propietario instance by example");
 		try {
 			List results = sessionFactory.getCurrentSession()
 					.createCriteria("negocio.Propietario")
 					.add(Example.create(instance)).list();
-			log.debug("find by example successful, result size: "
-					+ results.size());
 			return results;
 		} catch (RuntimeException re) {
-			log.error("find by example failed", re);
 			throw re;
 		}
 	}
--- src/dao/ReciboinmuebleHome.java
+++ src/dao/ReciboinmuebleHome.java
@@ -3,12 +3,11 @@ package dao;
 // Generated 21-mar-2013 18:13:29 by Hibernate Tools 3.4.0.CR1
 
 import java.util.List;
+
 import javax.naming.InitialContext;
 
 import negocio.Reciboinmueble;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.hibernate.LockMode;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Example;
@@ -20,7 +19,6 @@ import org.hibernate.criterion.Example;
  */
 public class ReciboinmuebleHome {
 
-	private static final Log log = LogFactory.getLog(ReciboinmuebleHome.class);
 
 	private final SessionFactory sessionFactory = getSessionFactory();
 
@@ -29,97 +27,73 @@ public class ReciboinmuebleHome {
 			return (SessionFactory) new InitialContext()
 					.lookup("SessionFactory");
 		} catch (Exception e) {
-			log.error("Could not locate SessionFactory in JNDI", e);
 			throw new IllegalStateException(
 					"Could not locate SessionFactory in JNDI");
 		}
 	}
 
 	public void persist(Reciboinmueble transientInstance) {
-		log.debug("persisting Reciboinmueble instance");
 		try {
 			sessionFactory.getCurrentSession().persist(transientInstance);
-			log.debug("persist successful");
 		} catch (RuntimeException re) {
-			log.error("persist failed", re);
 			throw re;
 		}
 	}
 
 	public void attachDirty(Reciboinmueble instance) {
-		log.debug("attaching dirty Reciboinmueble instance");
 		try {
 			sessionFactory.getCurrentSession().saveOrUpdate(instance);
-			log.debug("attach successful");
 		} catch (RuntimeException re) {
-			log.error("attach failed", re);
 			throw re;
 		}
 	}
 
 	public void attachClean(Reciboinmueble instance) {
-		log.debug("attaching clean Reciboinmueble instance");
 		try {
 			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
-			log.debug("attach successful");
 		} catch (RuntimeException re) {
-			log.error("attach failed", re);
 			throw re;
 		}
 	}
 
 	public void delete(Reciboinmueble persistentInstance) {
-		log.debug("deleting Reciboinmueble instance");
 		try {
 			sessionFactory.getCurrentSession().delete(persistentInstance);
-			log.debug("delete successful");
 		} catch (RuntimeException re) {
-			log.error("delete failed", re);
 			throw re;
 		}
 	}
 
 	public Reciboinmueble merge(Reciboinmueble detachedInstance) {
-		log.debug("merging Reciboinmueble instance");
 		try {
 			Reciboinmueble result = (Reciboinmueble) sessionFactory
 					.getCurrentSession().merge(detachedInstance);
-			log.debug("merge successful");
 			return result;
 		} catch (RuntimeException re) {
-			log.error("merge failed", re);
 			throw re;
 		}
 	}
 
 	public Reciboinmueble findById(java.lang.Integer id) {
-		log.debug("getting Reciboinmueble instance with id: " + id);
 		try {
 			Reciboinmueble instance = (Reciboinmueble) sessionFactory
 					.getCurrentSession().get("negocio.Reciboinmueble", id);
 			if (instance == null) {
-				log.debug("get successful, no instance found");
 			} else {
-				log.debug("get successful, instance found");
 			}
 			return instance;
 		} catch (RuntimeException re) {
-			log.error("get failed", re);
 			throw re;
 		}
 	}
 
 	public List findByExample(Reciboinmueble instance) {
-		log.debug("finding Reciboinmueble instance by example");
 		try {
 			List results = sessionFactory.getCurrentSession()
 					.createCriteria("negocio.Reciboinmueble")
 					.add(Example.create(instance)).list();
-			log.debug("find by example successful, result size: "
-					+ results.size());
 			return results;
 		} catch (RuntimeException re) {
-			log.error("find by example failed", re);
 			throw re;
 		}
 	}
