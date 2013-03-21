712e5304ea62683ba8b097e86ed00112094d8872
Carlos <zengo120@gmail.com>
2013-03-21 18:24:35 +0100

    eliminando logs

--- src/dao/ComunidadHome.java
+++ src/dao/ComunidadHome.java
@@ -3,12 +3,11 @@ package dao;
 // Generated 21-mar-2013 18:13:29 by Hibernate Tools 3.4.0.CR1
 
 import java.util.List;
+
 import javax.naming.InitialContext;
 
 import negocio.Comunidad;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.hibernate.LockMode;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Example;
@@ -20,7 +19,6 @@ import org.hibernate.criterion.Example;
  */
 public class ComunidadHome {
 
-	private static final Log log = LogFactory.getLog(ComunidadHome.class);
 
 	private final SessionFactory sessionFactory = getSessionFactory();
 
@@ -29,97 +27,73 @@ public class ComunidadHome {
 			return (SessionFactory) new InitialContext()
 					.lookup("SessionFactory");
 		} catch (Exception e) {
-			log.error("Could not locate SessionFactory in JNDI", e);
 			throw new IllegalStateException(
 					"Could not locate SessionFactory in JNDI");
 		}
 	}
 
 	public void persist(Comunidad transientInstance) {
-		log.debug("persisting Comunidad instance");
 		try {
 			sessionFactory.getCurrentSession().persist(transientInstance);
-			log.debug("persist successful");
 		} catch (RuntimeException re) {
-			log.error("persist failed", re);
 			throw re;
 		}
 	}
 
 	public void attachDirty(Comunidad instance) {
-		log.debug("attaching dirty Comunidad instance");
 		try {
 			sessionFactory.getCurrentSession().saveOrUpdate(instance);
-			log.debug("attach successful");
 		} catch (RuntimeException re) {
-			log.error("attach failed", re);
 			throw re;
 		}
 	}
 
 	public void attachClean(Comunidad instance) {
-		log.debug("attaching clean Comunidad instance");
 		try {
 			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
-			log.debug("attach successful");
 		} catch (RuntimeException re) {
-			log.error("attach failed", re);
 			throw re;
 		}
 	}
 
 	public void delete(Comunidad persistentInstance) {
-		log.debug("deleting Comunidad instance");
 		try {
 			sessionFactory.getCurrentSession().delete(persistentInstance);
-			log.debug("delete successful");
 		} catch (RuntimeException re) {
-			log.error("delete failed", re);
 			throw re;
 		}
 	}
 
 	public Comunidad merge(Comunidad detachedInstance) {
-		log.debug("merging Comunidad instance");
 		try {
 			Comunidad result = (Comunidad) sessionFactory.getCurrentSession()
 					.merge(detachedInstance);
-			log.debug("merge successful");
 			return result;
 		} catch (RuntimeException re) {
-			log.error("merge failed", re);
 			throw re;
 		}
 	}
 
 	public Comunidad findById(int id) {
-		log.debug("getting Comunidad instance with id: " + id);
 		try {
 			Comunidad instance = (Comunidad) sessionFactory.getCurrentSession()
 					.get("negocio.Comunidad", id);
 			if (instance == null) {
-				log.debug("get successful, no instance found");
 			} else {
-				log.debug("get successful, instance found");
 			}
 			return instance;
 		} catch (RuntimeException re) {
-			log.error("get failed", re);
 			throw re;
 		}
 	}
 
 	public List findByExample(Comunidad instance) {
-		log.debug("finding Comunidad instance by example");
 		try {
 			List results = sessionFactory.getCurrentSession()
 					.createCriteria("negocio.Comunidad")
 					.add(Example.create(instance)).list();
-			log.debug("find by example successful, result size: "
-					+ results.size());
 			return results;
 		} catch (RuntimeException re) {
-			log.error("find by example failed", re);
 			throw re;
 		}
 	}
--- src/dao/LineafacturaHome.java
+++ src/dao/LineafacturaHome.java
@@ -3,12 +3,11 @@ package dao;
 // Generated 21-mar-2013 18:13:29 by Hibernate Tools 3.4.0.CR1
 
 import java.util.List;
+
 import javax.naming.InitialContext;
 
 import negocio.Lineafactura;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.hibernate.LockMode;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Example;
@@ -20,7 +19,6 @@ import org.hibernate.criterion.Example;
  */
 public class LineafacturaHome {
 
-	private static final Log log = LogFactory.getLog(LineafacturaHome.class);
 
 	private final SessionFactory sessionFactory = getSessionFactory();
 
@@ -29,97 +27,73 @@ public class LineafacturaHome {
 			return (SessionFactory) new InitialContext()
 					.lookup("SessionFactory");
 		} catch (Exception e) {
-			log.error("Could not locate SessionFactory in JNDI", e);
 			throw new IllegalStateException(
 					"Could not locate SessionFactory in JNDI");
 		}
 	}
 
 	public void persist(Lineafactura transientInstance) {
-		log.debug("persisting Lineafactura instance");
 		try {
 			sessionFactory.getCurrentSession().persist(transientInstance);
-			log.debug("persist successful");
 		} catch (RuntimeException re) {
-			log.error("persist failed", re);
 			throw re;
 		}
 	}
 
 	public void attachDirty(Lineafactura instance) {
-		log.debug("attaching dirty Lineafactura instance");
 		try {
 			sessionFactory.getCurrentSession().saveOrUpdate(instance);
-			log.debug("attach successful");
 		} catch (RuntimeException re) {
-			log.error("attach failed", re);
 			throw re;
 		}
 	}
 
 	public void attachClean(Lineafactura instance) {
-		log.debug("attaching clean Lineafactura instance");
 		try {
 			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
-			log.debug("attach successful");
 		} catch (RuntimeException re) {
-			log.error("attach failed", re);
 			throw re;
 		}
 	}
 
 	public void delete(Lineafactura persistentInstance) {
-		log.debug("deleting Lineafactura instance");
 		try {
 			sessionFactory.getCurrentSession().delete(persistentInstance);
-			log.debug("delete successful");
 		} catch (RuntimeException re) {
-			log.error("delete failed", re);
 			throw re;
 		}
 	}
 
 	public Lineafactura merge(Lineafactura detachedInstance) {
-		log.debug("merging Lineafactura instance");
 		try {
 			Lineafactura result = (Lineafactura) sessionFactory
 					.getCurrentSession().merge(detachedInstance);
-			log.debug("merge successful");
 			return result;
 		} catch (RuntimeException re) {
-			log.error("merge failed", re);
 			throw re;
 		}
 	}
 
 	public Lineafactura findById(java.lang.Integer id) {
-		log.debug("getting Lineafactura instance with id: " + id);
 		try {
 			Lineafactura instance = (Lineafactura) sessionFactory
 					.getCurrentSession().get("negocio.Lineafactura", id);
 			if (instance == null) {
-				log.debug("get successful, no instance found");
 			} else {
-				log.debug("get successful, instance found");
 			}
 			return instance;
 		} catch (RuntimeException re) {
-			log.error("get failed", re);
 			throw re;
 		}
 	}
 
 	public List findByExample(Lineafactura instance) {
-		log.debug("finding Lineafactura instance by example");
 		try {
 			List results = sessionFactory.getCurrentSession()
 					.createCriteria("negocio.Lineafactura")
 					.add(Example.create(instance)).list();
-			log.debug("find by example successful, result size: "
-					+ results.size());
 			return results;
 		} catch (RuntimeException re) {
-			log.error("find by example failed", re);
 			throw re;
 		}
 	}
