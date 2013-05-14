19d18ad1a90317754e3298edd2653388f8d32981
Carlos <zengo120@gmail.com>
2013-05-14 20:11:30 +0200

    Añadida otra interfaz

--- src/interfazGrafica/GestionCartasGUI.java
+++ src/interfazGrafica/GestionCartasGUI.java
@@ -1,6 +1,11 @@
 package interfazGrafica;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.GroupLayout;
+import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 import javax.swing.LayoutStyle;
 
 import javax.swing.WindowConstants;
@@ -20,6 +25,12 @@ import javax.swing.SwingUtilities;
 * LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
 */
 public class GestionCartasGUI extends javax.swing.JFrame {
+	private JTextField jTF_IDCarta;
+	private JLabel jL_IDCarta;
+	private JTextField jTF_FechaCarta;
+	private JButton jB_GuardarCarta;
+	private JButton jB_CancelarCarta;
+	private JLabel jL_FechaCarta;
 
 	/**
 	* Auto-generated main method to display this JFrame
@@ -34,15 +45,90 @@ public class GestionCartasGUI extends javax.swing.JFrame {
 		try {
 			GroupLayout thisLayout = new GroupLayout((JComponent)getContentPane());
 			getContentPane().setLayout(thisLayout);
-			thisLayout.setHorizontalGroup(thisLayout.createParallelGroup());
-			thisLayout.setVerticalGroup(thisLayout.createParallelGroup());
 			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			{
+				jTF_IDCarta = new JTextField();
+			}
+			{
+				jL_IDCarta = new JLabel();
+				jL_IDCarta.setText("ID");
+			}
+			{
+				jTF_FechaCarta = new JTextField();
+			}
+			{
+				jL_FechaCarta = new JLabel();
+				jL_FechaCarta.setText("Fecha");
+			}
+			{
+				jB_CancelarCarta = new JButton();
+				jB_CancelarCarta.setText("Cancelar");
+				jB_CancelarCarta.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent evt) {
+						jB_CancelarCartaActionPerformed(evt);
+					}
+				});
+			}
+			{
+				jB_GuardarCarta = new JButton();
+				jB_GuardarCarta.setText("Guardar");
+				jB_GuardarCarta.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent evt) {
+						jB_GuardarCartaActionPerformed(evt);
+					}
+				});
+			}
+			thisLayout.setVerticalGroup(thisLayout.createSequentialGroup()
+				.addContainerGap(23, 23)
+				.addGroup(thisLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+				    .addComponent(jL_IDCarta, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
+				    .addComponent(jTF_IDCarta, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE))
+				.addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+				.addGroup(thisLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+				    .addComponent(jL_FechaCarta, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
+				    .addComponent(jTF_FechaCarta, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE))
+				.addGap(0, 0, Short.MAX_VALUE)
+				.addGroup(thisLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+				    .addComponent(jB_GuardarCarta, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)
+				    .addComponent(jB_CancelarCarta, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE))
+				.addContainerGap());
+			thisLayout.setHorizontalGroup(thisLayout.createSequentialGroup()
+				.addContainerGap(49, 49)
+				.addGroup(thisLayout.createParallelGroup()
+				    .addGroup(thisLayout.createSequentialGroup()
+				        .addGroup(thisLayout.createParallelGroup()
+				            .addComponent(jL_IDCarta, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE)
+				            .addComponent(jL_FechaCarta, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE))
+				        .addGap(103)
+				        .addGroup(thisLayout.createParallelGroup()
+				            .addGroup(thisLayout.createSequentialGroup()
+				                .addComponent(jTF_IDCarta, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE))
+				            .addGroup(thisLayout.createSequentialGroup()
+				                .addComponent(jTF_FechaCarta, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)))
+				        .addGap(0, 0, Short.MAX_VALUE))
+				    .addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
+				        .addGap(22)
+				        .addComponent(jB_GuardarCarta, GroupLayout.PREFERRED_SIZE, 101, GroupLayout.PREFERRED_SIZE)
+				        .addGap(42)
+				        .addComponent(jB_CancelarCarta, GroupLayout.PREFERRED_SIZE, 101, GroupLayout.PREFERRED_SIZE)
+				        .addGap(0, 27, Short.MAX_VALUE)))
+				.addContainerGap(48, 48));
 			pack();
 			setSize(400, 300);
 		} catch (Exception e) {
 		    //add your error handling code here
 			e.printStackTrace();
 		}
+	}
+	
+	private void jB_CancelarCartaActionPerformed(ActionEvent evt) {
+		System.out.println("jB_CancelarCarta.actionPerformed, event="+evt);
+		//TODO add your code for jB_CancelarCarta.actionPerformed
+	}
+	
+	private void jB_GuardarCartaActionPerformed(ActionEvent evt) {
+		System.out.println("jB_GuardarCarta.actionPerformed, event="+evt);
+		//TODO add your code for jB_GuardarCarta.actionPerformed
 	}
 
 }
